a887e791b5858ea017d920739b0319ad4fd2c995
uchiyamakoki <454768497@qq.com>
2019-04-07 15:56:57 +0800

    3-2 工程创建和插件安装

---
+++ Admin/Controller/src/main/java/com/tortuousroad/admin/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.tortuousroad.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: Gosin
+ * @Date: 2019/4/6 10:56
+ */
+public class LoginController {
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
